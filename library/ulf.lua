00a88b1b69341640c7dc278d55f51b0560daa8c0
SHBorg <al@shebang.org>
2024-09-17 20:29:42 +0200

    init

---
+++ library/ulf.lua
@@ -0,0 +1,12 @@
+---@meta
+---@diagnostic disable: duplicate-set-field
+local ulf
+
+---@class ulf.InitOptions
+
+---@class ulf
+---@field core ulf.core
+---@field doc ulf.doc
+---@field init fun(opts:ulf.InitOptions)
+---@field process ulf.GlobalProcess
+return ulf
